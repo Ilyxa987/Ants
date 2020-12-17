8bf8904d71b87d32fc1ee8c8e3ffa4897c60196d
Ilyxa987 <72066943+Ilyxa987@users.noreply.github.com>
2020-12-17 18:35:33 +0800

    Add files via upload

---
+++ core/src/com/mygdx/game/Armor.java
@@ -0,0 +1,5 @@
+package com.mygdx.game;
+
+public class Armor extends Items {
+    int defence;
+}
---
+++ core/src/com/mygdx/game/Consumables.java
@@ -0,0 +1,6 @@
+package com.mygdx.game;
+
+public class Consumables extends Items {
+    int quanity;
+    void Use(){}
+}
