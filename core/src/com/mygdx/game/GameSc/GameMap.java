be85039ba0f324de165659733b3cdae177e6e2ca
Ilyxa987 <72066943+Ilyxa987@users.noreply.github.com>
2021-01-28 17:36:29 +0800

    Five

--- core/src/com/mygdx/game/GameSc/GameMap.java
+++ core/src/com/mygdx/game/GameSc/GameMap.java
@@ -1,7 +1,6 @@
 package com.mygdx.game.GameSc;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
