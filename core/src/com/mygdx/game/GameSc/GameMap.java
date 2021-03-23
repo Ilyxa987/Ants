851c6e513ab7a349537966f3b2e4ed21f444d7ec
Ilyxa987 <72066943+Ilyxa987@users.noreply.github.com>
2021-03-23 20:51:10 +0800

    Баги с картой

--- core/src/com/mygdx/game/GameSc/GameMap.java
+++ core/src/com/mygdx/game/GameSc/GameMap.java
@@ -4,6 +4,10 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthoCachedTiledMapRenderer;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.mygdx.game.SkillsPack.AttackSkill;
@@ -20,22 +24,22 @@ public class GameMap extends Stage {
     int textureSize = 32;
     Texture spriteOne, spriteTwo;
     final Games games;
-    int mapArray[][];
     float height = Gdx.graphics.getHeight(), width = Gdx.graphics.getWidth();
-    float x, y;
     Player player;
     Enemy enemy;
-    Vector3 touchPos;
+    Vector3 touchPos, scroll;
     OrthographicCamera camera;
     SkillBar skillBar;
     public ArrayList<Units> unitsArray;
     public Units activeUnit;
+    TiledMap map;
+    OrthoCachedTiledMapRenderer renderer;
 
 
     public GameMap(Games games) {
         this.games = games;
 
-        player = new Player("Player", Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2, this);
+        player = new Player("Player", 0, 0, this);
         enemy = new Enemy("Enemy", Gdx.graphics.getWidth() / 3 * 2, Gdx.graphics.getHeight() / 2, this, player);
         unitsArray = new ArrayList<>();
         unitsArray.add(player);
@@ -46,43 +50,25 @@ public class GameMap extends Stage {
 
         camera = new OrthographicCamera();
         touchPos = new Vector3();
+        scroll = new Vector3();
         camera.unproject(touchPos);
+        camera.setToOrtho(false, 1200, 720); // При изменении этого параметра выходит BufferOverFlowException
+        camera.update();
 
+        map = new TmxMapLoader().load("mapp.tmx");
 
-        spriteOne = new Texture("spriteOne.png");
-        spriteTwo = new Texture("spriteTwo.png");
-
-        mapArray = new int[(int) height / textureSize][(int) width / textureSize];
-
-        for (int i = 0; i < mapArray.length; i++) {
-            for (int j = 0; j < mapArray[i].length; j++) {
-                if (i <= mapArray.length / 4 || j <= mapArray[i].length / 4 || i >= mapArray.length / 4 * 3 || j >= mapArray[i].length / 4 * 3)
-                    mapArray[i][j] = 1;
-                else
-                    mapArray[i][j] = 0;
-            }
-        }
+        renderer = new OrthoCachedTiledMapRenderer(map);
     }
 
 
     @Override
     public void draw() {
         super.draw();
-        x = 0;
-        y = 0;
         SpriteBatch batch = new SpriteBatch();
         batch.begin();
-        for (int i = 0; i < mapArray.length; i++) {
-            for (int j = 0; j < mapArray[i].length; j++) {
-                if (mapArray[i][j] == 1)
-                    batch.draw(spriteTwo, x, y);
-                else
-                    batch.draw(spriteOne, x, y);
-                x += textureSize;
-            }
-            y += textureSize;
-            x = 0;
-        }
+        camera.update();
+        renderer.setView(camera);
+        renderer.render();
         if (activeUnit.Move) {
             if (activeUnit == player)
                 activeUnit.Move(touchPos.x, touchPos.y);
@@ -116,18 +102,26 @@ public class GameMap extends Stage {
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        System.out.println("HHHHHHHHHHHHHHHHHHHHHHHHHHHHHH");
+        System.out.println("Coordinates Camera " + camera.position.x + " " + camera.position.y);
         touchPos.set(screenX, screenY, 0);
         if (touchPos.x >= enemy.getX() && AttackSkill.attack == true){
             System.out.println("RRRRRRR");
-                player.Attack = true;
-                player.Move = false;
+            player.Attack = true;
+            player.Move = false;
         }
         else {
             player.Attack = false;
             player.Move = true;
         }
         return true;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        screenX = (int) (screenX + (camera.position.x - 329.11108));
+        screenY = (int) (screenY + ((camera.position.y - 160) / 1048209));
+        camera.position.set(Gdx.graphics.getWidth() - screenX, screenY, camera.position.z);
+        return super.touchDragged(screenX, screenY, pointer);
     }
 
     public Games getGames() {
